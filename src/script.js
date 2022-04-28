a339ae4d45c81b2f7971f6fddbe4bf1adfb0b87b
Emircan Ok <emircanok@gmail.com>
2022-04-28 19:37:43 +0300

    some updates

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.to('.side', {
+    gsap.to('#right-square', {
         scrollTrigger: {
             trigger: '#s-1',
             markers: true,
@@ -44,28 +44,31 @@ document.addEventListener("DOMContentLoaded", function () {
             start: "top top",
             toggleActions: 'play pause resume reverse',
             scrub: 1,
-            pin: true
+            pin: 'main'
         }
     })
         // .to('#point-1 > path', { fill: '#FEC267' })
         .to('#point-2 > path', { fill: '#FEC267' })
-        .to('#progress', { height: '25%' }, '<')
+        .to('#progress', { height: '+=25%' }, '<')
         .to('#pointer', { top: '+=25%' }, '<')
-        .to('#text-1', { display: 'none', opacity: 0, y: '-20' })
-        .to('#text-2', { display: 'block', opacity: 1, y: '-20' })
+        .to('#text-1, #text-box-1', { display: 'none', opacity: 0, y: '-20' })
+        .to('#text-2, #text-box-2', { display: 'block', opacity: 1, y: '-20' })
         .to('#point-3 > path', { fill: '#FEC267' })
-        .to('#progress', { height: '50%' }, '<')
+        .to('#progress', { height: '+=25%' }, '<')
         .to('#pointer', { top: '+=25%' }, '<')
-        .to('#text-2', { display: 'none', opacity: 0, y: '-20' })
-        .to('#text-3', { display: 'block', opacity: 1, y: '-20' })
+        .to('#text-2, #text-box-2', { display: 'none', opacity: 0, y: '-20' })
+        .to('#text-3, #text-box-3', { display: 'block', opacity: 1, y: '-20' })
         .to('#point-4 > path', { fill: '#FEC267' })
-        .to('#progress', { height: '75%' }, '<')
+        .to('#progress', { height: '+=25%' }, '<')
         .to('#pointer', { top: '+=25%' }, '<')
-        .to('#text-3', { display: 'none', opacity: 0, y: '-20' })
-        .to('#text-4', { display: 'block', opacity: 1, y: '-20' })
+        .to('#text-3, #text-box-3', { display: 'none', opacity: 0, y: '-20' })
+        .to('#text-4, #text-box-4', { display: 'block', opacity: 1, y: '-20' })
         .to('#point-5 > path', { fill: '#FEC267' })
-        .to('#progress', { height: '100%' }, '<')
+        .to('#progress', { height: '+=25%' }, '<')
         .to('#pointer', { top: '+=25%' }, '<')
+        .to('#s-3', { yPercent: '-100' }, '<')
+        .to('.point', { background: '#FEC267' }, '<')
+        // .to('#s-2', { background: '#020202' }, '<')
         ;
 
 });
