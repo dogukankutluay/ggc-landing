a3c36f7c027405be34ce7594126e38c22c1b4ef1
yagizanil14 <58178879+yagizanil14@users.noreply.github.com>
2022-06-03 21:27:15 +0300

    Son hali
    Son hali

---
+++ src/main.js
@@ -0,0 +1,18 @@
+
+function videobtn1() {
+    var videoscreen = $("#videoscreen");
+    videoscreen.attr("src", "img/GGCM_Videos/1)_Howsign_up_to_backoffice.mov");
+    videoscreen.load();
+}
+
+function videobtn2() {
+    var videoscreen = $("#videoscreen");
+    videoscreen.attr("src", "img/GGCM_Videos/4) How to create deposit address.mov");
+    videoscreen.load();
+}
+
+function videobtn3() {
+    var videoscreen = $("#videoscreen");
+    videoscreen.attr("src", "img/GGCM_Videos/6) How to buy GGCM Tokens Part 2.mp4");
+    videoscreen.load();
+}
