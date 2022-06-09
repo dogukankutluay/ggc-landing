5d80d2308dfaf055792089a3153ab5f2b72e54f2
yagizanil14 <58178879+yagizanil14@users.noreply.github.com>
2022-06-09 19:57:06 +0300

    Son Değişiklikler
    Son Değişiklikler

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 
 function videobtn1() {
     var videoscreen = $("#videoscreen");
-    videoscreen.attr("src", "img/GGCM_Videos/1)_Howsign_up_to_backoffice.mov");
+    videoscreen.attr("src", "img/GGCM_Videos/sign up.mp4");
     videoscreen.load();
 }
 
 function videobtn2() {
     var videoscreen = $("#videoscreen");
-    videoscreen.attr("src", "img/GGCM_Videos/4) How to create deposit address.mov");
+    videoscreen.attr("src", "img/GGCM_Videos/deposit address.mp4");
     videoscreen.load();
 }
 
 function videobtn3() {
     var videoscreen = $("#videoscreen");
-    videoscreen.attr("src", "img/GGCM_Videos/6) How to buy GGCM Tokens Part 2.mp4");
+    videoscreen.attr("src", "img/GGCM_Videos/buy ggcm token.mp4");
     videoscreen.load();
 }
